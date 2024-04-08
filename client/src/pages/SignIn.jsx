43cfa649f21cc4083d466a9de76943c37205c42f
SSashinn <sayedayaan226@gmail.com>
2024-04-08 17:52:17 +0530

    created signup and signin ui

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,22 @@
+import {Link} from 'react-router-dom';
+
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div className="auth-container">
+      <h2>Welcome Back</h2>
+      <form className="auth-form" method="POST">
+        <div className="auth-input-div">
+          <label>USERNAME</label>
+          <input type="text" name="username" />
+        </div>
+        <div className="auth-input-div">
+          <label>PASSWORD</label>
+          <input type="password" name="password" />
+        </div>
+       
+        <button type="button" className="submit-form">Continue</button>
+      </form>
+      <Link to={'/signup'}><p className="auth-footer">Need an account?</p></Link>
+    </div>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,22 @@
+import {Link} from 'react-router-dom';
+
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="auth-container">
+      <h2>Create an account</h2>
+      <form className="auth-form" method="POST">
+        <div className="auth-input-div">
+          <label>USERNAME</label>
+          <input type="text" name="username" />
+        </div>
+        <div className="auth-input-div">
+          <label>PASSWORD</label>
+          <input type="password" name="password" />
+        </div>
+       
+        <button type="button" className="submit-form">Continue</button>
+      </form>
+      <Link to={'/signin'}><p className="auth-footer">Already have an account?</p></Link>
+    </div>
   )
 }
