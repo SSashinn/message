781991535b2c2d812b8c47dfeb679485cad23ae8
SSashinn <sayedayaan226@gmail.com>
2024-04-14 16:26:52 +0530

    Added sidebar for DM

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h3>Direct Message</h3>
+      <p>random stuff</p>
+      <p>random stuff</p>
+      <p>random stuff</p>
+      <p>random stuff</p>
+      <p>random stuff</p>
+      <p>random stuff</p>
+      <p>random stuff</p>
+    </div>
+  )
+}
